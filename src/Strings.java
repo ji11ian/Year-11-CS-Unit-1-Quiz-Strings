8b7cd38d54e1938fd2ce113c8eb33f4c488a36ad
ji11ian <123039281+ji11ian@users.noreply.github.com>
2023-02-20 09:36:40 +0800

    Update Strings.java

--- src/Strings.java
+++ src/Strings.java
@@ -3,25 +3,26 @@ public class Strings {
     public static void main(String[] args) {
 
         // 1.
-
+        int age = 33;
         // 2.
-
+        String name = "T Swizzle";
         // 3.
-
+        System.out.println(age);
         // 4.
-
+        System.out.println(name);
         // 5.
-
+        String description = "My name is " + name + " and I am " + age + " years old.";
         // 6.
-
+        System.out.println(description);
         // 7.
-
+        System.out.println(description.length());
         // 8.
-
+        String nameSubDescription = description.substring(11, 20);
         // 9.
-
+        int ageSubDescription = Integer.parseInt(description.substring(32, 34));
         // 10.
-
+        String subDescription = "My name is " + nameSubDescription + " and I am " + ageSubDescription + " years old.";
+       
     }
 
 }
